f6ce0b6e36bd46161ce2e3dc761aebf54493df03
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-07-27 21:45:44 +0200

    feat: print info about given hosts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use anyhow::Result;
 
 use crate::ssh::shell_cfg::ShellCfg;
@@ -6,8 +8,22 @@ mod ssh;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("kame probe!");
-    let cfg = ShellCfg::new("google.com").await?;
-    println!("hostname: {:?}", cfg.get("sendenv"));
+    for host in env::args().skip(1) {
+        let cfg = ShellCfg::new(&host).await?;
+        println!(">>> {host}");
+        print_field(&cfg, "hostname");
+        print_field(&cfg, "user");
+        print_field(&cfg, "port");
+        print_field(&cfg, "controlmaster");
+        println!();
+    }
     Ok(())
+}
+
+fn print_field(shell_cfg: &ShellCfg, field: &str) {
+    println!(
+        "{}: {:?}",
+        field,
+        shell_cfg.get(field).unwrap_or(&["???".to_owned()])
+    );
 }
