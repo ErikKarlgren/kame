09c1a1bb94c47b7fc7ee196b3d07e239d3483155
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-08-09 01:43:39 +0200

    feat: actually run subcommands (only pick is starting to be implemented)

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,11 @@
 #![deny(clippy::perf)]
 #![deny(clippy::nursery)]
 
-use anyhow::anyhow;
-use std::env;
-
 use anyhow::Result;
 
 use crate::{
-    cli::parse_args,
-    ssh::{host_config::HostConfig, host_finder::parse_hosts},
+    cli::{Subcommand, parse_args},
+    pick::pick,
 };
 
 mod cli;
@@ -25,45 +22,10 @@ mod ssh;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _cli = parse_args();
-    let tasks: Vec<_> = env::args()
-        .skip(1)
-        .map(|host| {
-            tokio::spawn(async move {
-                let cfg = HostConfig::parse(&host).await;
-                (host, cfg)
-            })
-        })
-        .collect();
-
-    if tasks.is_empty() {
-        println!("Parsing hosts");
-        let hosts = parse_hosts(
-            dirs::home_dir()
-                .ok_or_else(|| anyhow!("no home dir found"))?
-                .join(".ssh/config"),
-        )
-        .await?;
-        for h in hosts {
-            println!("{h}");
-        }
-        return Ok(());
-    }
-
-    for task in tasks {
-        let (host, cfg) = task.await?;
-        let cfg = cfg?;
-        println!(">>> {host}");
-        print_field(&cfg, "hostname");
-        print_field(&cfg, "user");
-        print_field(&cfg, "port");
-        print_field(&cfg, "controlmaster");
-        println!();
+    let cli = parse_args();
+    match cli.command {
+        Subcommand::Pick(pick_args) => pick(pick_args).await?,
+        Subcommand::Probe(_probe_args) => todo!(),
     }
     Ok(())
-}
-
-fn print_field(cfg: &HostConfig, field: &str) {
-    let default = &["???".to_owned()];
-    println!("{}: {:?}", field, cfg.get(field).unwrap_or(default));
 }
