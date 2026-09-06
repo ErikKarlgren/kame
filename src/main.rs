417073e42c2ca764e84b4d66ce8e8c982645c89b
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-09-06 15:13:06 +0200

    clippy: move lints config from main.rs to Cargo.toml and clippy.toml

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Erik Karlgren Domercq
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-#![warn(clippy::cargo)]
-#![deny(clippy::style)]
-#![deny(clippy::complexity)]
-#![deny(clippy::perf)]
-#![deny(clippy::nursery)]
-#![deny(clippy::unwrap_used)]
-#![deny(clippy::expect_used)]
-
 use anyhow::Result;
 
 use crate::{
